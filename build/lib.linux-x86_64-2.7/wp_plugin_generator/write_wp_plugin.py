eccd5af340f7201acb3bd23663e1c0507de1cf13
Richard Royal <richard@saidigital.co>
2013-03-05 17:43:41 -0500

    Added to ignore file.

--- build/lib.linux-x86_64-2.7/wp_plugin_generator/write_wp_plugin.py
+++ build/lib.linux-x86_64-2.7/wp_plugin_generator/write_wp_plugin.py
@@ -6,5 +6,15 @@ def parse_arguments():
   """
   Loads and checks arguments from STDIN
   """
-  print "crap"
+
+  # Grab command line arguments
+  parser = argparse.ArgumentParser(description='Script that uses a config file to generate a WordPress plugin scaffold.')
+  parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='an integer for the accumulator')
+  args = parser.parse_args()
+  
+ 
+  stream = open( args.infile.name )
+  config = yaml.load(stream)
+  
+  config['setup']
 
